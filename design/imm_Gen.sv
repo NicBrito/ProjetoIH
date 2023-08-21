b6ed7e4475b5398bf4ab9839826c872c36ffd04a
Alex Candido <acs11@cin.ufpe.br>
2023-08-20 21:48:14 -0300

    SLLI coisa

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -11,9 +11,16 @@ module imm_Gen (
       7'b0000011:  /*I-type load part*/
       Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
 
-      7'b0100011:  /*S-type*/
-      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:25], inst_code[11:7]};
+      7'b0010011: begin/*ADDI-SLTI*/ /*SLLI-SRLI-SRAI*/
+      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+        if(inst_code[14:12] == 3'b000 || inst_code[14:12] == 3'b010) begin
 
+          Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+
+        end else if(inst_code[14:12] == 3'b001 || inst_code[14:12] == 3'b101)begin  
+              Imm_out = {27'b0 ,inst_code[24:20]};
+            end
+      end
       7'b1100011:  /*B-type*/
       Imm_out = {
         inst_code[31] ? 19'h7FFFF : 19'b0,
